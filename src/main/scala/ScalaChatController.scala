05f79c0fce15895fb04ddfe128f95600444563ba
ElderHex <karasevrobert@gmail.com>
2022-03-21 10:47:20 +0400

    Naming revisions 0.02

--- src/main/scala/ScalaChatController.scala
+++ src/main/scala/ScalaChatController.scala
@@ -1,5 +1,6 @@
 import User.{ExitUser, JsonSerializable, PrivateMsg, PublicMsg, SetUsername, WelcomeUser}
 import akka.actor.typed.ActorSystem
+import akka.actor.typed.javadsl.Behaviors
 import akka.cluster.typed.Cluster
 import com.typesafe.config.ConfigFactory
 import javafx.application.Platform
